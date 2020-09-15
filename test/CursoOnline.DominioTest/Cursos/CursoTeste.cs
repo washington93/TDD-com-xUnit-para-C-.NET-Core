c944a717da2a8a7fbaa36566fed06a3a9aef082e
Washington Silva <washington.s@escolar.ifrn.edu.br>
2020-09-15 01:05:51 -0300

    Initial Structure

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTeste.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTeste
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            string nome = "Informática básica";
+            double cargaHoraria = 80;
+            string publicoAlvo = "Estudantes";
+            double valor = 950;
+
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, curso.nome);
+            Assert.Equal(cargaHoraria, curso.cargaHoraria);
+            Assert.Equal(publicoAlvo, curso.publicoAlvo);
+            Assert.Equal(valor, curso.valor);
+        }
+    }
+
+    internal class Curso
+    {
+
+
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        {
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.publicoAlvo = publicoAlvo;
+            this.valor = valor;
+        }
+
+        public string nome { get; set; }
+        public double cargaHoraria { get; set; }
+        public string publicoAlvo { get; set; }
+        public double valor { get; set; }
+    }
+}
