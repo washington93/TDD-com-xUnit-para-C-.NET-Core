dba69959558b1feab622f4ef152a61f907ffc231
Washington Silva <washington.s@escolar.ifrn.edu.br>
2020-10-09 11:47:17 -0300

    Melhorando os testes

---
+++ src/CursoOnline.Dominio/Cursos/Curso.cs
@@ -0,0 +1,35 @@
+﻿using CursoOnline.Dominio.Cursos;
+using System;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+   
+    public class Curso
+    {
+
+        public Curso(string nome, string descricao, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome inválido");
+
+            if (cargaHoraria < 1)
+                throw new ArgumentException("Carga horária inválida");
+
+            if (valor < 0)
+                throw new ArgumentException("Valor inválido");
+
+            this.nome = nome;
+            this.descricao = descricao;
+            this.cargaHoraria = cargaHoraria;
+            this.publicoAlvo = publicoAlvo;
+            this.valor = valor;
+        }
+
+        public string nome { get; private set; }
+        public string descricao { get; private set; }
+        public double cargaHoraria { get; private set; }
+        public PublicoAlvo publicoAlvo { get; private set; }
+        public double valor { get; private set; }
+    }
+
+}
---
+++ test/CursoOnline.DominioTest/_Util/AssertExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace CursoOnline.DominioTest._Util
+{
+    public static class AssertExtension
+    {
+        public static void ComMensagem(this ArgumentException execption, string mensagem)
+        {
+            if(execption.Message == mensagem)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.False(true, $"Esperava a mensagem '{mensagem}'");
+            }
+        }
+    }
+}
